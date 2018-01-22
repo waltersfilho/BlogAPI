ce8ef9bead9b65392955ded4a6d8a94b087b1e4e
Walter Filho <wspfilho@gmail.com>
2018-01-22 18:01:22 -0200

    auth api

--- routes/api.php
+++ routes/api.php
@@ -13,17 +13,24 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => 'auth:api'], function(){
+    Route::post('details', 'api\UserController@details');
+});
+Route::group(['middleware' => 'auth:api'], function (){
+    Route::get('/post', 'PostsController@index');
+    Route::post('/post/new', 'PostsController@store');
+    Route::post('post/update/{id}', 'PostsController@update');
+    Route::get('post/{id}', 'PostsController@show');
+    Route::get('post/destroy/{id}', 'PostsController@destroy');
+
+    Route::post('/post/comments/new', 'CommentsController@store');
+    Route::post('post/comments/update/{id}', 'CommentsController@update');
+    Route::get('post/comments/{id}', 'CommentsController@show');
+    Route::get('post/comments/destroy/{id}', 'CommentsController@destroy');
+
+    Route::post('logout','api\UserController@logout');
 });
 
-Route::get('/post', 'PostsController@index');
-Route::post('/post/new', 'PostsController@store');
-Route::post('post/update/{id}', 'PostsController@update');
-Route::get('post/{id}', 'PostsController@show');
-Route::get('post/destroy/{id}', 'PostsController@destroy');
 
-Route::post('/post/comments/new', 'CommentsController@store');
-Route::post('post/comments/update/{id}', 'CommentsController@update');
-Route::get('post/comments/{id}', 'CommentsController@show');
-Route::get('post/comments/destroy/{id}', 'CommentsController@destroy');
+Route::post('login', 'api\UserController@login');
+Route::post('register', 'api\UserController@register');
